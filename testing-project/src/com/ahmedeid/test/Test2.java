2a65f39a39e418fa04e2d611d3a4435c3b070960
ahmed.eid <ahmed.eid@mswks097.itblocks.local>
2020-12-20 12:11:20 +0200

    hamada

---
+++ testing-project/src/com/ahmedeid/test/Test2.java
@@ -0,0 +1,11 @@
+package com.ahmedeid.test;
+
+public class Test2 {
+
+	public static String test;
+	
+	public static String testing() {
+		Test2.test = "ahmed eid";
+		return test;
+	}
+}
--- testing-project/src/com/ahmedeid/test/Testing.java
+++ testing-project/src/com/ahmedeid/test/Testing.java
@@ -3,7 +3,7 @@ package com.ahmedeid.test;
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("ahmed eid");
+		System.out.println(Test2.testing());
 	}
 
 }
